9d2605392615b5cc40d42a1aeaddc7285c0802af
Administrator <1025973129@qq.com>
2020-05-10 21:15:56 +0800

    添加测试Test

---
+++ src/com/test/Test.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * @Author 朱屹
+ * @Date 2020/5/10 21:14
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这是我测试默认格式...");
+    }
+}
