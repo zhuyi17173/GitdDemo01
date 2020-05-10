f9551a0b04d5c2eadab9bf81b389e6046c168c14
Administrator <1025973129@qq.com>
2020-05-10 22:01:38 +0800

    123456

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -6,6 +6,8 @@ package com.test;
  * @Version 1.0
  */
 public class Test {
+    private String address;
+
     public static void main(String[] args) {
         System.out.println("这是我测试默认格式...");
         System.out.println("这是些内容已被另一个程序员修改了...");
